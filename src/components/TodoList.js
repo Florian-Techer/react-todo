064a0929c6da12a8d9b5902679efe9f00f4ca035
Florian Techer <techerflorian1@gmail.com>
2023-03-29 23:26:03 +0200

    localstorage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,17 +9,21 @@ function TodoList(){
 
     useEffect(() => {
         if(localStorage.getItem('todoList')){
+            console.log(JSON.parse(localStorage.getItem('todoList')))
             setTodos(JSON.parse(localStorage.getItem('todoList')))
+            setTaskDone(JSON.parse(localStorage.getItem('taskDone')))    
         }
-    },[])
+        console.log("b")   
+    }, [])
 
-    const setTodosLocalStorage = (todoList) => {
-        localStorage.setItem('todoList', JSON.stringify(todoList));
+    const setLocalStorage = (key, value) => {
+        window.localStorage.setItem(key, JSON.stringify(value));
     }
 
     const addTodo = todo => {
         const newTodos = [...todos, todo];
-        setTodosLocalStorage(newTodos)
+        setLocalStorage('todoList', newTodos)
+        setLocalStorage('taskDone', taskDone)
         setTodos(newTodos)
     }
     
@@ -32,10 +36,12 @@ function TodoList(){
         const removeArray = [...todos].filter(todo => {
             if(todo.id === id && todo.isComplete){
                 setTaskDone(taskDone - 1)
+                setLocalStorage('taskDone', taskDone)
             }
             return todo.id !== id
         });
-        setTodosLocalStorage(removeArray)
+        setLocalStorage('todoList', removeArray)
+        setLocalStorage('taskDone', taskDone)
         setTodos(removeArray);
     }
 
@@ -46,15 +52,19 @@ function TodoList(){
                 todo.isComplete = !todo.isComplete
                 if(todo.isComplete){
                     setTaskDone(taskDone + 1)
+                    setLocalStorage('taskDone', taskDone+1)
                 }else if(!todo.isComplete){
                     setTaskDone(taskDone - 1)
+                    setLocalStorage('taskDone', taskDone-1)
                 }
             }
             return todo
         })
-        setTodosLocalStorage(updatedTodos)
+        setLocalStorage('todoList', updatedTodos)
+        
         setTodos(updatedTodos);
     }
+    
      return <div>
                 <h1>What's the plan for today ?</h1>
                 <div id='counter-container'>
